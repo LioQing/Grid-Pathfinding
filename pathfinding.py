6d8c2117dd7238154de0e82c5960ac5405b01b60
LioQing <46854695+LioQing@users.noreply.github.com>
2023-04-14 18:09:47 +0800

    Change to_be_visited to discovered

--- pathfinding.py
+++ pathfinding.py
@@ -5,10 +5,10 @@ from typing import Generator
 
 
 def depth_first_search(board: list[list[Cell]], start: Cell, end: Cell) -> Generator[list[Cell], None, None]:
-    # to_be_visited = a set containing start cell
+    # discovered = a set containing start cell
     #
-    # while end cell is not visited and to_be_visited is not empty:
-    #     let u = pop the latest cell from to_be_visited
+    # while end cell is not visited and discovered is not empty:
+    #     let u = pop the latest cell from discovered
     #
     #     u.visited = True
     #
@@ -18,12 +18,12 @@ def depth_first_search(board: list[list[Cell]], start: Cell, end: Cell) -> Gener
     #     for each neighbor v of u:
     #         if v is not a wall and not visited:
     #             let v.parent = next
-    #             add v to the end of to_be_visited
+    #             add v to the end of discovered
 
-    to_be_visited = [start]
+    discovered = [start]
 
-    while not end.visited and len(to_be_visited) > 0:
-        u = to_be_visited.pop()
+    while not end.visited and len(discovered) > 0:
+        u = discovered.pop()
 
         u.visited = True
 
@@ -33,9 +33,9 @@ def depth_first_search(board: list[list[Cell]], start: Cell, end: Cell) -> Gener
         for v in u.get_neighbours(board):
             if not v.visited:
                 v.parent = u
-                if v in to_be_visited:
-                    to_be_visited.remove(v)
-                to_be_visited.append(v)
+                if v in discovered:
+                    discovered.remove(v)
+                discovered.append(v)
 
         yield u.get_path()
 
@@ -46,10 +46,10 @@ def depth_first_search(board: list[list[Cell]], start: Cell, end: Cell) -> Gener
 
 
 def breadth_first_search(board: list[list[Cell]], start: Cell, end: Cell) -> Generator[list[Cell], None, None]:
-    # to_be_visited = a set containing start cell
+    # discovered = a set containing start cell
     #
-    # while end cell is not visited and to_be_visited is not empty
-    #     let u = pop the earliest cell from to_be_visited
+    # while end cell is not visited and discovered is not empty
+    #     let u = pop the earliest cell from discovered
     #
     #     u.visited = True
     #
@@ -59,12 +59,12 @@ def breadth_first_search(board: list[list[Cell]], start: Cell, end: Cell) -> Gen
     #     for each neighbor v of u:
     #         if v is not a wall and not visited:
     #             let v.parent = next
-    #             add v to the end of to_be_visited
+    #             add v to the end of discovered
 
-    to_be_visited = [start]
+    discovered = [start]
 
-    while not end.visited and len(to_be_visited) > 0:
-        u = to_be_visited.pop(0)
+    while not end.visited and len(discovered) > 0:
+        u = discovered.pop(0)
 
         u.visited = True
 
@@ -72,9 +72,9 @@ def breadth_first_search(board: list[list[Cell]], start: Cell, end: Cell) -> Gen
             break
 
         for v in u.get_neighbours(board):
-            if not v.visited and v not in to_be_visited:
+            if not v.visited and v not in discovered:
                 v.parent = u
-                to_be_visited.append(v)
+                discovered.append(v)
 
         yield u.get_path()
 
@@ -85,12 +85,12 @@ def breadth_first_search(board: list[list[Cell]], start: Cell, end: Cell) -> Gen
 
 
 def dijkstra(board: list[list[Cell]], start: Cell, end: Cell) -> Generator[list[Cell], None, None]:
-    # to_be_visited = a sorted set containing start cell
+    # discovered = a sorted set containing start cell
     # set all cells' total cost to infinity
     # set start cell's total cost to 0
     #
-    # while end cell is not visited and to_be_visited is not empty:
-    #     let u = pop the lowest total cost cell from to_be_visited
+    # while end cell is not visited and discovered is not empty:
+    #     let u = pop the lowest total cost cell from discovered
     #
     #     u.visited = True
     #
@@ -102,9 +102,9 @@ def dijkstra(board: list[list[Cell]], start: Cell, end: Cell) -> Generator[list[
     #         if v is not visited and new_cost < v.total_cost:
     #             let v.parent = next
     #             let v.total_cost = new_cost
-    #             add v to to_be_visited
+    #             add v to discovered
 
-    to_be_visited = {start}
+    discovered = {start}
 
     for row in board:
         for cell in row:
@@ -112,9 +112,9 @@ def dijkstra(board: list[list[Cell]], start: Cell, end: Cell) -> Generator[list[
 
     start.total_cost = 0
 
-    while not end.visited and len(to_be_visited) > 0:
-        u = sorted(to_be_visited, key=lambda c: c.total_cost)[0]
-        to_be_visited.remove(u)
+    while not end.visited and len(discovered) > 0:
+        u = sorted(discovered, key=lambda c: c.total_cost)[0]
+        discovered.remove(u)
 
         u.visited = True
 
@@ -126,7 +126,7 @@ def dijkstra(board: list[list[Cell]], start: Cell, end: Cell) -> Generator[list[
             if not v.visited and new_cost < v.total_cost:
                 v.parent = u
                 v.total_cost = new_cost
-                to_be_visited.add(v)
+                discovered.add(v)
 
         yield u.get_path()
 
@@ -137,7 +137,7 @@ def dijkstra(board: list[list[Cell]], start: Cell, end: Cell) -> Generator[list[
 
 
 def a_star(board: list[list[Cell]], start: Cell, end: Cell) -> Generator[list[Cell], None, None]:
-    # to_be_visited = a sorted set containing start cell
+    # discovered = a sorted set containing start cell
     # set all cells' total cost to infinity
     # set all cells' dist to infinity
     #
@@ -146,8 +146,8 @@ def a_star(board: list[list[Cell]], start: Cell, end: Cell) -> Generator[list[Ce
     # set start cell's total cost to its heuristic
     # set start cell's dist to 0
     #
-    # while end cell is not visited and to_be_visited is not empty:
-    #     let u = pop the lowest total cost cell from to_be_visited
+    # while end cell is not visited and discovered is not empty:
+    #     let u = pop the lowest total cost cell from discovered
     #
     #     u.visited = True
     #
@@ -160,9 +160,9 @@ def a_star(board: list[list[Cell]], start: Cell, end: Cell) -> Generator[list[Ce
     #             let v.parent = next
     #             let v.total_cost = new_cost
     #             let v.dist = u.dist + cost of moving to v
-    #             add v to to_be_visited
+    #             add v to discovered
 
-    to_be_visited = {start}
+    discovered = {start}
 
     for row in board:
         for cell in row:
@@ -174,9 +174,9 @@ def a_star(board: list[list[Cell]], start: Cell, end: Cell) -> Generator[list[Ce
     start.total_cost = start.heuristic
     start.dist = 0
 
-    while not end.visited and len(to_be_visited) > 0:
-        u = sorted(to_be_visited, key=lambda c: c.total_cost)[0]
-        to_be_visited.remove(u)
+    while not end.visited and len(discovered) > 0:
+        u = sorted(discovered, key=lambda c: c.total_cost)[0]
+        discovered.remove(u)
 
         u.visited = True
 
@@ -189,7 +189,7 @@ def a_star(board: list[list[Cell]], start: Cell, end: Cell) -> Generator[list[Ce
                 v.parent = u
                 v.total_cost = new_cost
                 v.dist = u.dist + v.get_cost()
-                to_be_visited.add(v)
+                discovered.add(v)
 
         yield u.get_path()
 
